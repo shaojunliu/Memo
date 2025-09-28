8aac56f19ba1cdaabbfc479bf6336343ef7ca526
shaojunliu <liu_shaojun@yeah.net>
2025-09-28 19:44:22 +0800

    auth

--- src/main/java/org/Memo/Config/SecurityConfig.java
+++ src/main/java/org/Memo/Config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {        http
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .headers(h -> h.frameOptions(f -> f.disable()));
 
